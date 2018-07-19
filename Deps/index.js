fdec6d42833a23bc60e6db9861cde794faf1d21e
hellomrbigshot <1994yechen@gmail.com>
2018-07-20 00:04:02 +0800

    update

--- Deps/index.js
+++ Deps/index.js
@@ -51,7 +51,25 @@ function defineReactive (obj, key, val) {
         enumerable: true,
         configurable: true,
         get: function reactiveGetter () {
-            /*   */
+            /* 将 Dep.target (当前的 Watcher 对象) 存入 dep 的 subs 中  */
+            dep.addSub(Dep.target);
+            return val;
+        },
+        set: function reactiveSetter (newVal) {
+            if (newVal === val) return;
+            /* 在 set 的时候触发 dep 的 notify 来通知所有的 Watcher 对象更新视图 */
+            dep.notify();
         }
     })
+}
+
+class Vue {
+    constructor (options) {
+        this._data = options.data;
+        observer(this._data); // observer 方法见 defineProperty/index.js
+        /* 新建一个 Watcher 观察者对象，这时候 Dep.target 指向这个 Watcher 对象 */
+        new Watcher();
+        /* 在这里模拟 render 的过程，为了触发 test 属性的 get 函数 */
+        console.log('render', this._data.test);
+    }
 }
